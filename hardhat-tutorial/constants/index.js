ac501713813e3cf76e01790b533e9fba679a78a2
Jack Chong <jack.chong@chch.ox.ac.uk''git config --global user.name Jack>
2022-03-04 12:36:06 +0000

    First commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x307b6e6171E2a9cdF740B395F14A8EC256298Abc";
+
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
