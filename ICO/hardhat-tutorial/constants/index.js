6c093d65cb48b8aa3cbf1f3b8b99a8a693f4e818
Jack Chong <jack.chong@chch.ox.ac.uk''git config --global user.name Jack>
2022-03-05 00:06:42 +0000

    First commit

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x9e3C205A042Bb4A7C71a7DA2C6dB1Ae20bc56516";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
